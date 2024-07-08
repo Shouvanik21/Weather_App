b1622ce48ca36abe0508dd7056f170566c368c97
Shouvanik21 <shouvanik210305@gmail.com>
2024-07-08 18:39:57 +0530

    updated commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const app=document.querySelector(".weather_app");
-const temp=document.querySelector(".temp");
+const temp=document.querySelector('.temp');
 const dateOutput=document.querySelector(".date");
 const timeOutput=document.querySelector(".time");
 const conditionOutput=document.querySelector(".condition");
@@ -13,15 +13,15 @@ const search=document.querySelector(".search");
 const btn=document.querySelector(".submit");
 const cities=document.querySelectorAll(".city");
 
-let cityInput="kolkata";
+let cityInput="London";
 
 cities.forEach((city) =>{
     city.addEventListener('click', (e) =>{
         cityInput=e.target.innerHTML;
 
-        fetchWeatherData();
+        fetchWeatherData(cityInput);
 
-        app.style.opacity="0";
+        app.style.opacity="1";
     })
 })
 
@@ -32,7 +32,7 @@ form.addEventListener('submit', (e)=>{
     else{
         cityInput=search.value;
 
-        fetchWeatherData();
+        fetchWeatherData(cityInput);
 
         search.value="";
 
@@ -47,8 +47,8 @@ function dayOfTheWeek(day,month,year){
     return weekday[new Date(`${day}/${month}/${year}`).getDay()];
 };
 
-function fetchWeatherData(){
-    fetch(`http://api.weatherapi.com/v1/current.json?key=fc07bcdce45c4b8a8d3211322240607&q=${cityInput}`)
+function fetchWeatherData(cityInput){
+    fetch(`https://api.weatherapi.com/v1/current.json?key=fc07bcdce45c4b8a8d3211322240607&q=${cityInput}`)
 
     .then(response => response.json())
     .then(data=>{
@@ -57,22 +57,22 @@ function fetchWeatherData(){
         temp.innerHTML = data.current.temp_c+ "&#176;";
         conditionOutput.innerHTML=data.current.condition.text;
 
-        const date= date.location.localtime;
+        const date= data.location.localtime;
         const y=parseInt(date.substr(0,4));
         const m=parseInt(date.substr(5,2));
         const d=parseInt(date.substr(8,2));
         const time=date.substr(11);
 
-        dateOutput.innerHTML=`${dayOfTheWeek(d,m,y)} ${d}, ${m} ${y}`;
+        dateOutput.innerHTML=`${dayOfTheWeek(d, m, y)} ${d}, ${m} ${y}`;
         timeOutput.innerHTML=time;
 
         nameOutput.innerHTML=data.location.name;
 
-        const iconId=data.current.condition.icon.substr("//cdn.weatherapi.com/weather/64x64/".length);
+        // const iconId=date.current.condition.icon.substr("//cdn.weatherapi.com/weather/64x64/".length);
 
-        icon.src="./icons/"+iconId;
+        // icon.src="./icons/" + iconId;
 
-        cloudOutput.innerHTML= data.current.cloud+ "%";
+        cloudOutput.innerHTML= data.current.cloud + "%";
         humidityOutput.innerHTML=data.current.humidity+ "%";
         windOutput.innerHTML=data.current.wind_kph+ "km/h";
 
@@ -84,35 +84,44 @@ function fetchWeatherData(){
         }
 
         if(code==1000){
-            app.style.backgroundImage=`url(./images/${timeOfDay}/clear.jpg)`;
+            app.style.backgroundImage=`url(images/${timeOfDay}/clear.jpg)`;
 
             btn.style.background="#e5ba92";
             if(timeOfDay=="night"){
                 btn.style.background="#181e27";
             }
+
+            app.style.opacity="1";
         }
+
         else if(code==1003 || code==1006 || code==1009 || code==1030 || code==1069 || code==1087 || code==1135 || code==1273 || code==1276 || code==1279 || code==1282){
-            app.style.backgroundImage=`url(.images/${timeOfDay}/cloudy.jpg)`;
+            app.style.backgroundImage=`url(images/${timeOfDay}/cloudy.jpg)`;
             btn.style.background="#fa6d1b";
             if(timeOfDay=="night"){
                 btn.style.background="#181e27";
             }
+
+            app.style.opacity="1";
         }
+
         else if(code==1063 || code==1069 || code==1072 || code==1150 || code==1153 || code==1180 || code==1183 || code==1186 || code==1189 || code==1192 || code==1195 || code==1204 || code==1207 || code==1240 || code==1243 || code==1246 || code==1249 || code==1252){
-            app.style.backgroundImage=`url(./images/${timeOfDay}/rainy.jpg)`;
+            app.style.backgroundImage=`url(images/${timeOfDay}/rainy.jpg)`;
 
             btn.style.background="#647d75";
 
             if(timeOfDay=="night"){
                 btn.style.background="#325c80";
-            }
-            else{
-                app.style.backgroundImage=`url(./images/${timeOfDay}/snowy.jpg)`;
+            }         
+            
+            app.style.opacity="1";
+        }
 
-                btn.style.background="#4d72aa";
-                if(timeOfDay=="night"){
-                    btn.style.background="#1b1b1b";
-                }
+        else{
+            app.style.backgroundImage=`url(images/${timeOfDay}/snowy.jpg)`;
+
+            btn.style.background="#4d72aa";
+            if(timeOfDay=="night"){
+                btn.style.background="#1b1b1b";
             }
 
             app.style.opacity="1";
@@ -125,6 +134,6 @@ function fetchWeatherData(){
     })
 }
 
-fetchWeatherData();
+fetchWeatherData(cityInput);
 
 app.style.opacity="1";
